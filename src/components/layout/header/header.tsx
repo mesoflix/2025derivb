da8e6389cfaf584e8c18943856dc9cb3801920c4
Matthew Mataruse <mtthmatthie@gmail.com>
2025-04-21 17:59:49 +0200

    Initial commit

---
+++ src/components/layout/header/header.tsx
@@ -0,0 +1,175 @@
+import clsx from 'clsx';
+import { observer } from 'mobx-react-lite';
+import { standalone_routes } from '@/components/shared';
+import Button from '@/components/shared_ui/button';
+import useActiveAccount from '@/hooks/api/account/useActiveAccount';
+import useIsGrowthbookIsLoaded from '@/hooks/growthbook/useIsGrowthbookLoaded';
+import { useApiBase } from '@/hooks/useApiBase';
+import { useStore } from '@/hooks/useStore';
+import { StandaloneCircleUserRegularIcon } from '@deriv/quill-icons/Standalone';
+import { requestOidcAuthentication } from '@deriv-com/auth-client';
+import { Localize, useTranslations } from '@deriv-com/translations';
+import { Header, useDevice, Wrapper } from '@deriv-com/ui';
+import { Tooltip } from '@deriv-com/ui';
+import { isDotComSite } from '../../../utils';
+import { AppLogo } from '../app-logo';
+import AccountsInfoLoader from './account-info-loader';
+import AccountSwitcher from './account-switcher';
+import MenuItems from './menu-items';
+import MobileMenu from './mobile-menu';
+import PlatformSwitcher from './platform-switcher';
+import './header.scss';
+
+const AppHeader = observer(() => {
+    const { isGBLoaded, isGBAvailable } = useIsGrowthbookIsLoaded();
+    const { isDesktop } = useDevice();
+    const { isAuthorizing, activeLoginid } = useApiBase();
+    const { client } = useStore() ?? {};
+
+    const { data: activeAccount } = useActiveAccount({ allBalanceData: client?.all_accounts_balance });
+    const { accounts, getCurrency } = client ?? {};
+    const has_wallet = Object.keys(accounts ?? {}).some(id => accounts?.[id].account_category === 'wallet');
+
+    const currency = getCurrency?.();
+    const { localize } = useTranslations();
+
+    const renderAccountSection = () => {
+        if (isAuthorizing) {
+            return <AccountsInfoLoader isLoggedIn isMobile={!isDesktop} speed={3} />;
+        } else if (activeLoginid) {
+            return (
+                <>
+                    {/* <CustomNotifications /> */}
+                    {isDesktop &&
+                        (() => {
+                            const redirect_url = new URL(standalone_routes.personal_details);
+                            // Check if the account is a demo account
+                            // Use the URL parameter to determine if it's a demo account, as this will update when the account changes
+                            const urlParams = new URLSearchParams(window.location.search);
+                            const account_param = urlParams.get('account');
+                            const is_virtual = client?.is_virtual || account_param === 'demo';
+
+                            if (is_virtual) {
+                                // For demo accounts, set the account parameter to 'demo'
+                                redirect_url.searchParams.set('account', 'demo');
+                            } else if (currency) {
+                                // For real accounts, set the account parameter to the currency
+                                redirect_url.searchParams.set('account', currency);
+                            }
+                            return (
+                                <Tooltip
+                                    as='a'
+                                    href={redirect_url.toString()}
+                                    tooltipContent={localize('Manage account settings')}
+                                    tooltipPosition='bottom'
+                                    className='app-header__account-settings'
+                                >
+                                    <StandaloneCircleUserRegularIcon className='app-header__profile_icon' />
+                                </Tooltip>
+                            );
+                        })()}
+                    <AccountSwitcher activeAccount={activeAccount} />
+                    {isDesktop &&
+                        (has_wallet ? (
+                            <Button
+                                className='manage-funds-button'
+                                has_effect
+                                text={localize('Manage funds')}
+                                onClick={() => {
+                                    let redirect_url = new URL(standalone_routes.wallets_transfer);
+
+                                    if (isGBAvailable && isGBLoaded) {
+                                        redirect_url = new URL(standalone_routes.recent_transactions);
+                                    }
+
+                                    if (currency) {
+                                        redirect_url.searchParams.set('account', currency);
+                                    }
+                                    window.location.assign(redirect_url.toString());
+                                }}
+                                primary
+                            />
+                        ) : (
+                            <Button
+                                primary
+                                onClick={() => {
+                                    const redirect_url = new URL(standalone_routes.cashier_deposit);
+                                    if (currency) {
+                                        redirect_url.searchParams.set('account', currency);
+                                    }
+                                    window.location.assign(redirect_url.toString());
+                                }}
+                                className='deposit-button'
+                            >
+                                {localize('Deposit')}
+                            </Button>
+                        ))}
+                </>
+            );
+        } else {
+            return (
+                <div className='auth-actions'>
+                    <Button
+                        tertiary
+                        onClick={async () => {
+                            const getQueryParams = new URLSearchParams(window.location.search);
+                            const currency = getQueryParams.get('account') ?? '';
+                            const query_param_currency =
+                                sessionStorage.getItem('query_param_currency') || currency || 'USD';
+                            try {
+                                if (isDotComSite()) {
+                                    await requestOidcAuthentication({
+                                        redirectCallbackUri: `${window.location.origin}/callback`,
+                                        ...(query_param_currency
+                                            ? {
+                                                  state: {
+                                                      account: query_param_currency,
+                                                  },
+                                              }
+                                            : {}),
+                                    }).catch(err => {
+                                        // eslint-disable-next-line no-console
+                                        console.error(err);
+                                    });
+                                }
+                            } catch (error) {
+                                // eslint-disable-next-line no-console
+                                console.error(error);
+                            }
+                        }}
+                    >
+                        <Localize i18n_default_text='Log in' />
+                    </Button>
+                    <Button
+                        primary
+                        onClick={() => {
+                            window.open(standalone_routes.signup);
+                        }}
+                    >
+                        <Localize i18n_default_text='Sign up' />
+                    </Button>
+                </div>
+            );
+        }
+    };
+
+    return (
+        <Header
+            className={clsx('app-header', {
+                'app-header--desktop': isDesktop,
+                'app-header--mobile': !isDesktop,
+            })}
+        >
+            <Wrapper variant='left'>
+                <AppLogo />
+                <MobileMenu />
+                {isDesktop && <MenuItems.TradershubLink />}
+                {isDesktop && <PlatformSwitcher />}
+                {isDesktop && <MenuItems />}
+            </Wrapper>
+            <Wrapper variant='right'>{renderAccountSection()}</Wrapper>
+        </Header>
+    );
+});
+
+export default AppHeader;
